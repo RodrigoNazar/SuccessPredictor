6017808e69931c5ad75a7cc1c4bd5e8a8ab1e2fa
Rodrigo Nazar <rnazar@uc.cl>
2021-12-02 15:48:16 -0300

    bugfix

--- src/utils/SpotifyApi.py
+++ src/utils/SpotifyApi.py
@@ -2,10 +2,6 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import json
 
-SPOTIPY_CLIENT_ID = '1bd287021f34474799d1e87d14bd77b8'
-SPOTIPY_CLIENT_SECRET = 'ad02f5ddb016462f915637070a3b89df'
-SPOTIPY_REDIRECT_URI = 'http://localhost:8080'
-
 scope = "user-library-read"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
@@ -14,35 +10,6 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     SPOTIPY_REDIRECT_URI,
     scope=scope
 ))
-
-'''
-results = sp.current_user_saved_tracks()
-
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
-
-
-playlists = sp.user_playlists('matiasaravena1')
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
-
-lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
-
-results = sp.artist_top_tracks(lz_uri)
-
-for track in results['tracks'][:10]:
-    print('track    : ' + track['name'])
-    print('audio    : ' + track['preview_url'])
-    print('cover art: ' + track['album']['images'][0]['url'])
-    print()
-
-'''
 
 lz_uri = '37i9dQZF1DX7Jl5KP2eZaS'
 
