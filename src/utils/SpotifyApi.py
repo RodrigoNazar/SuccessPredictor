fd1ac8064d275c930815993a2fba7b13f4b02750
Rodrigo Nazar <rnazar@uc.cl>
2021-12-06 02:23:35 -0300

    last pdf version

--- src/utils/SpotifyApi.py
+++ src/utils/SpotifyApi.py
@@ -2,38 +2,51 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import json
 
+# with open('./CREDENTIALS.txt', 'r') as f:
+#     credentials = json.loads(f.read())
+
+credentials = {
+  "SPOTIPY_CLIENT_ID": "1bd287021f34474799d1e87d14bd77b8",
+  "SPOTIPY_CLIENT_SECRET": "ad02f5ddb016462f915637070a3b89df",
+  "SPOTIPY_REDIRECT_URI": "http://localhost:8080"
+}
+
+
 scope = "user-library-read"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    SPOTIPY_CLIENT_ID,
-    SPOTIPY_CLIENT_SECRET,
-    SPOTIPY_REDIRECT_URI,
+    credentials['SPOTIPY_CLIENT_ID'],
+    credentials['SPOTIPY_CLIENT_SECRET'],
+    credentials['SPOTIPY_REDIRECT_URI'],
     scope=scope
 ))
 
+scope = "user-library-read"
+
 lz_uri = '37i9dQZF1DX7Jl5KP2eZaS'
 
-results = sp.playlist_items(lz_uri)
-
-for i in results['items']:
-    '''album, artists, available_markets, disc_number, duration_ms, episode,
-    explicit, external_ids, external_urls, href, id, is_local, name, popularity,
-    preview_url, track, track_number, type, uri
-    '''
-    # print(i)
-    for j in i['track']:
-        print(i['track']['name'])
-        print(i['track']['href'])
-
-        print(sp.audio_features(i['track']['href']))
-
-
-
-        # print(i['track']['track_number'])
-        #     for w in j.keys():
-        #         print(w)
-        #         break
-        break
-    # break
+results = sp.audio_features('1AnJQd8TfkhK6M3VgZDVv1')
+print(json.dumps(results, indent=2))
+#
+# for i in results['items']:
+#     '''album, artists, available_markets, disc_number, duration_ms, episode,
+#     explicit, external_ids, external_urls, href, id, is_local, name, popularity,
+#     preview_url, track, track_number, type, uri
+#     '''
+#     # print(i)
+#     for j in i['track']:
+#         print(i['track']['name'])
+#         print(i['track']['href'])
+#
+#         print(sp.audio_features(i['track']['href']))
+#
+#
+#
+#         # print(i['track']['track_number'])
+#         #     for w in j.keys():
+#         #         print(w)
+#         #         break
+#         break
+#     # break
 
 # print(json.dumps([i['preview_url'] for i in results['tracks']], indent=2))
